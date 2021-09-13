7867c4266654273444974e8051862fefe8156d74
wu zaiqiang <wuzaiqiang@zju.edu.cn>
2021-09-13 21:26:25 +0800

    modify checkerboard

--- src/include/Checkerboard.hpp
+++ src/include/Checkerboard.hpp
@@ -14,7 +14,7 @@ public:
     GLfloat height, width;
     CheckerBoard()
     {
-        Scale(50);
+        Scale(150);
         Rotate(3.1415926/2, glm::vec3(1.0f, 0.0f, 0.0f));
         Translate(glm::vec3(0,0,0.05));
         myShader = NULL;
